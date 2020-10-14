7a19d5cc7745b96a1dc78733061b64c4341a2534
xianghao <xianghaowang2001@outlook.com>
2020-10-14 21:21:08 +0800

    Add ReportPanel

---
+++ src/com/company/gui/panel/ReportPanel.java
@@ -0,0 +1,21 @@
+package com.company.gui.panel;
+
+import com.company.gui.util.ChartUtil;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ReportPanel extends JPanel {
+    public static ReportPanel instance = new ReportPanel();
+
+    public JLabel chart = new JLabel();
+
+    public ReportPanel() {
+        this.setLayout(new BorderLayout());
+
+        ImageIcon imageIcon = new ImageIcon(ChartUtil.getImage(400, 300));
+        chart.setIcon(imageIcon);
+
+        this.add(chart, BorderLayout.CENTER);
+    }
+}
