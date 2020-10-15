19667e2b650b5ec8aedbb8c57c70dda3bc15a001
xianghao <xianghaowang2001@outlook.com>
2020-10-15 20:50:06 +0800

    Add CategoryDAO

---
+++ src/com/company/dao/CategoryDAO.java
@@ -0,0 +1,151 @@
+package com.company.dao;
+
+import com.company.entity.Category;
+import com.company.util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryDAO implements DAO<Category> {
+
+    @Override
+    public void add(Category obj) {
+        String sql = "INSERT INTO category VALUE (NULL, ?)";
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql)) {
+
+            ps.setString(1, obj.getName());
+            ps.execute();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                int id = rs.getInt(1);
+                obj.setId(id);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void delete(Category obj) {
+        String sql = "DELETE FROM category WHERE id=?";
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql)) {
+
+            int id = obj.getId();
+            ps.setInt(1, id);
+            ps.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void update(Category obj) {
+        String sql = "UPDATE category SET name=? WHERE id=?";
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql)) {
+
+            int id = obj.getId();
+            String name = obj.getName();
+
+            ps.setString(1, name);
+            ps.setInt(2, id);
+
+            ps.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Category get(int id) {
+        Category category = null;
+
+        String sql = "SELECT * FROM category WHERE id=?";
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql)) {
+
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                String name = rs.getString("name");
+
+                category = new Category();
+                category.setId(id);
+                category.setName(name);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return category;
+    }
+
+    @Override
+    public List<Category> list() {
+        return list(0, Integer.MAX_VALUE);
+    }
+
+    @Override
+    public List<Category> list(int start, int count) {
+        List<Category> items = new ArrayList<>();
+
+        String sql = "SELECT * FROM category ORDER BY id DESC LIMIT ?, ?";
+
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql)) {
+
+            ps.setInt(1, start);
+            ps.setInt(2, count);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+
+                Category category = new Category();
+                category.setId(id);
+                category.setName(name);
+
+                items.add(category);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return items;
+    }
+
+    @Override
+    public int count() {
+        int total = 0;
+
+        String sql = "SELECT COUNT(*) FROM category";
+        try (Connection c = DBUtil.getConnection();
+             PreparedStatement ps = c.prepareStatement(sql)) {
+
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                total = rs.getInt(1);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return total;
+    }
+}
