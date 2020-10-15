18ef2e667e6966ab2db44e9c5eedfbb3638d4a8e
xianghao <xianghaowang2001@outlook.com>
2020-10-15 10:07:49 +0800

    Add entity package and its classes

---
+++ src/com/company/entity/Record.java
@@ -0,0 +1,51 @@
+package com.company.entity;
+
+import java.util.Date;
+
+public class Record {
+    private int id;
+    private int spend;
+    private int category_id;
+    private String comment;
+    private Date date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSpend() {
+        return spend;
+    }
+
+    public void setSpend(int spend) {
+        this.spend = spend;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
